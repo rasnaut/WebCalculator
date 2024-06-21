231b8a53d5d360b3e78a3be9b71ff0472be02000
IvanBurniashev <kbk@ukr.net>
2024-06-21 12:33:04 +0200

    add persent/add, persent/multiple

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -1,13 +1,27 @@
 package com.example.j_calculator;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/j_calculator")
 public class CalculatorController {
-    
+
     @GetMapping("/add")
     public String add(@RequestParam int a, @RequestParam int b) {
         return String.valueOf(a + b);
     }
+
+    @GetMapping("/persent/add")
+    public String persentAdd(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a + a * b / 100);
+    }
+
+    @GetMapping("/persent/multiple")
+    public String persentMultiple(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a * b / 100);
+    }
+
 }
