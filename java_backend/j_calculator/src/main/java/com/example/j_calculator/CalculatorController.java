45acc1f77de39ec8a983461b7e8c42ddac1e5457
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-08-01 12:13:51 +0200

    added new Task for issue 39

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -25,4 +25,9 @@ public class CalculatorController {
     public String substruct(@RequestParam int a, @RequestParam int b) {
         return String.valueOf(a - b);
     }
+    
+    @GetMapping("/squaring")
+    public String squaring(@RequestParam int a) {
+        return String.valueOf(a * a);
+    }
 }
